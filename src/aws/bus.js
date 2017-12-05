4480d4450fa07e77e671646b4bf09beec1e8c64d
hotcakedev628 <hotcake.dev.628@protonmail.com>
2017-12-05 16:37:43 +0200

    Fix bus

---
+++ src/aws/bus.js
@@ -0,0 +1,26 @@
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-west-2' });
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+var params = {
+    DelaySeconds: 10,
+    MessageAttributes: {},
+    MessageBody: "Information about current NY Times fiction bestseller for week of 12/11/2016.",
+    QueueUrl: process.env.AWS_QUEUE_URL
+};
+
+
+
+
+function send(commandName, payload) {
+    return new Promise((resolve, reject) => {
+        sqs.sendMessage(params, function (err, data) {
+            params.MessageAttributes['command'] = commandName
+            params.MessageBody = payload
+            if (err) reject(err);
+            else resolve(data.MessageId);
+        });
+    })
+}
+
+
+module.exports = { send }
--- src/controllers/accounts.ctrl.js
+++ src/controllers/accounts.ctrl.js
@@ -1,23 +1,25 @@
-
+const bus = require('../aws/bus')
 const commands = require('../config/commands.constants')
 module.exports = {
     createAccount(req, res, next) {
-        defaultRespone(commands.createAccount)
+        defaultRespone(req.body, res, next, commands.createAccount)
     },
     deleteAccount(req, res, next) {
-        defaultRespone(commands.deleteAccount)
+        defaultRespone(req.body, res, next, commands.deleteAccount)
     },
     reinstateAccount(req, res, next) {
-        defaultRespone(commands.reinstateAccount)
+        defaultRespone(req.body, res, next, commands.reinstateAccount)
     },
     updateAccountAddress(req, res, next) {
-        defaultRespone(commands.updateAccountAddress)
+        defaultRespone(req.body, res, next, commands.updateAccountAddress)
     },
     approveAccount(req, res, next) {
-        defaultRespone(commands.approveAccount)
+        defaultRespone(req.body, res, next, commands.approveAccount)
     },
 }
 
-function defaultRespone(commandName) {
-    await bus.send(defaultRespone).catch()
+async function defaultRespone(payload, res, next,commandName) {
+    const result = await bus.send(commandName, payload)
+    if (result) return res.status(202).send('We accepeted your request. Please check again soon if everything is Ok')
+    else return res.status(400).json('You could not perform this request')
 }
